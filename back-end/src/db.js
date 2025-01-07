540162d6d9fabeab9a26f4fc44094b41e57ed9f7
Stefano Zucarelli <stefanozuca@gmail.com>
2025-01-07 17:16:40 -0300

    Update database conn

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
+    client = await MongoClient.connect('mongodb+srv://stefanozuca:IYPsPPbQh4HbpBrv@cluster0.rt1xd.mongodb.net/', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
